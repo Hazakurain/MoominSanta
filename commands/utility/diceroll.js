111f239dde420984a0aa7a955344c4f98cf23015
Tristan Da Costa Pinto <tristandacostapinto@gmail.com>
2024-11-06 14:58:39 +0100

    Diceroll now has an image and is embed

--- commands/utility/diceroll.js
+++ commands/utility/diceroll.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const { AttachmentBuilder, SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,12 +9,37 @@ module.exports = {
         const diceNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
         const randomRoll = diceNumber[Math.floor(Math.random()*diceNumber.length)];
 
+        const colour = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
+        const randColour = colour[Math.floor(Math.random() * colour.length)];
+        const picture = new AttachmentBuilder("./assets/img/D20WHITE.png")
+        const diceRollEmbed = new EmbedBuilder()
+            .setColor(randColour)
+            .setThumbnail('attachment://D20WHITE.png')
+            .setTitle(`You rolled the dice and ...`)
+            .setDescription(`You rolled ${randomRoll} !`)
+
+
         if(randomRoll === "1"){
-            await interaction.reply("Critical fail ! You rolled a 1 !");
+            const diceRollEmbed = new EmbedBuilder()
+                .setColor(randColour)
+                .setThumbnail('attachment://D20WHITE.png')
+                .setTitle(`You rolled the dice and ...`)
+                .setDescription(`You rolled 1 ! Oh no ... :(`)
+            await interaction.reply({embeds: [diceRollEmbed], files: [picture]});
         } else if (randomRoll === "20"){
-            await interaction.reply("Critical Success ! You rolled a 20 !");
+            const diceRollEmbed = new EmbedBuilder()
+                .setColor(randColour)
+                .setThumbnail('attachment://D20WHITE.png')
+                .setTitle(`You rolled the dice and ...`)
+                .setDescription(`You rolled 20 ! Critical success !`)
+            await interaction.reply({embeds: [diceRollEmbed], files: [picture]});
         } else {
-            await interaction.reply(`You rolled ${randomRoll}`);
+            const diceRollEmbed = new EmbedBuilder()
+                .setColor(randColour)
+                .setThumbnail('attachment://D20WHITE.png')
+                .setTitle(`You rolled the dice and ...`)
+                .setDescription(`You rolled ${randomRoll} !`)
+            await interaction.reply({embeds: [diceRollEmbed], files: [picture]});
         }
 
     }
