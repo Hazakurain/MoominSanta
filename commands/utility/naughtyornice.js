c1c700d6be553179b69551ef1835dcf23871275b
Tristan Da Costa Pinto <tristandacostapinto@gmail.com>
2024-11-06 16:58:48 +0100

    Changement naughtyornice pour mettre un embed et une image

--- commands/utility/naughtyornice.js
+++ commands/utility/naughtyornice.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -16,23 +16,62 @@ module.exports = {
             if(interaction.member.roles.cache.some(role => role.name === "NaughtyPatootie")){
                 await interaction.member.roles.remove("1303372723023777822");
                 await interaction.member.roles.add("1303372685547536475");
-                await interaction.reply ("Alright ... alright... I'm putting you on the cutie list");
+                const picture = new AttachmentBuilder("./assets/img/happymin.png")
+                const cutiePatootieEmbed = new EmbedBuilder()
+                    .setColor("DarkGreen")
+                    .setThumbnail('attachment://happymin.png')
+                    .setTitle(`🎄 Alright ... alright ... 🎄`)
+                    .setDescription(`I'm putting you on the cutie list 😇 !`)
+                await interaction.reply({embeds: [cutiePatootieEmbed], files: [picture]});
             } else if(interaction.member.roles.cache.some(role => role.name === "CutiePatootie")) {
-                await interaction.reply("You are already on the cutie list ! ");
+                const picture = new AttachmentBuilder("./assets/img/happymin.png")
+                const cutiePatootieEmbed = new EmbedBuilder()
+                    .setColor("DarkGreen")
+                    .setThumbnail('attachment://happymin.png')
+                    .setTitle(`🎄 Hey !  🎄`)
+                    .setDescription(`You are already on the cutie list ! 😇`)
+                await interaction.reply({embeds: [cutiePatootieEmbed], files: [picture]});
             } else {
                 await interaction.member.roles.add("1303372685547536475");
-                await interaction.reply ("You are a sweet beautiful child");
+                const picture = new AttachmentBuilder("./assets/img/happymin.png")
+                const cutiePatootieEmbed = new EmbedBuilder()
+                    .setColor("DarkGreen")
+                    .setThumbnail('attachment://happymin.png')
+                    .setTitle(`🎄 Moomin santa went through the list... 🎄`)
+                    .setDescription(`His eyes spots you are on the cutie patootie list! 😇 !`)
+
+                await interaction.reply({embeds: [cutiePatootieEmbed], files: [picture]});
+
             }
         } else {
             if(interaction.member.roles.cache.some(role => role.name === "CutiePatootie")){
                 await interaction.member.roles.remove("1303372685547536475");
                 await interaction.member.roles.add("1303372723023777822");
-                await interaction.reply ("SINCE YOU ARE COMPLAINING I'M PUTTING YOU ON THE NAUGHTY LIST");
+                const picture = new AttachmentBuilder("./assets/img/ANGRY.png")
+                const naughtyPatootieEmbed = new EmbedBuilder()
+                    .setColor("DarkRed")
+                    .setThumbnail('attachment://ANGRY.png')
+                    .setTitle(`Ewww ! Stinky !`)
+                    .setDescription(`👺 SINCE YOU ARE COMPLAINING I'M PUTTING YOU ON THE NAUGHTY LIST 👺`)
+                await interaction.reply({embeds: [naughtyPatootieEmbed], files: [picture]});
             } else if(interaction.member.roles.cache.some(role => role.name === "NaughtyPatootie")) {
-                await interaction.reply("You are already on the naughty list ! ");
+                const picture = new AttachmentBuilder("./assets/img/ANGRY.png")
+                const naughtyPatootieEmbed = new EmbedBuilder()
+                    .setColor("DarkRed")
+                    .setThumbnail('attachment://ANGRY.png')
+                    .setTitle(`Why ?`)
+                    .setDescription(`You are already on the naughty list !` )
+                await interaction.reply({embeds: [naughtyPatootieEmbed], files: [picture]});
             } else {
                 await interaction.member.roles.add("1303372723023777822");
-                await interaction.reply ("You've been a terrible child, havent you ? ");
+                const picture = new AttachmentBuilder("./assets/img/stinky.png")
+                const naughtyPatootieEmbed = new EmbedBuilder()
+                    .setColor("DarkRed")
+                    .setThumbnail('attachment://stinky.png')
+                    .setTitle(`🎄 Moomin santa went through the list... 🎄`)
+                    .setDescription(`His eyes spots you are on the naughty patootie list! 😈`)
+
+                await interaction.reply({embeds: [naughtyPatootieEmbed], files: [picture]});
                 }
             }
     }
